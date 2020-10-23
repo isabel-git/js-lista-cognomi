c0916dc278b2477799ce4f77d5343e53bb910796
Isabel <isabel.gogov1@libero.it>
2020-10-23 17:17:48 +0200

    arrey ordinato

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+var bvai = document.getElementById("vai");
 var arreyNonOrd = "";
 var outputCognNonOrd = document.getElementById("listaNonOrd");
-var bvai = document.getElementById("vai");
+var arreyOrd = "";
+var contenitore;
+var outputCognOrd = document.getElementById("listaOrd");
 
 // arrey dei cognomi
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
@@ -20,13 +23,29 @@ bvai.addEventListener("click",
       i++;
       outputCognNonOrd.innerHTML = arreyNonOrd;
     }
-    // azzero il contatore
-
 
     // ordinare l'arrey
+    i = 0; // azzero il contatore
+    // confrontiamo a coppie l'elemento del arrey selezionato con quello successivo
+    while (i < (cognomi.length - 1)) {
+      // confrontiamo se bianchi e' maggiore di rossi
+      if (cognomi[i] > cognomi[i + 1]) {
+        contenitore = cognomi[i]; // con la variabile d'appoggio salvo il valore della variabile che voglio sovrascrivere
+        cognomi[i] = cognomi[i + 1]; // sovrascrivo bianchi con rossi
+        cognomi[i + 1] = contenitore; // sovrascrivendo rossi con il valore salvato nella variabili d'appoggio che e bianchi
+        // scambiando l'elemento lo faccio ripartire dell'elemento in posizione zero
+        i = 0;
+      }
+      i++;
+    }
 
-
-    // output arrey ordinato
+    // output arrey ordinata
+    var i = 0;
+    while (i < cognomi.length) {
+      arreyOrd = arreyOrd + "<li>" + cognomi[i] + "</li>";
+      i++;
+      outputCognOrd.innerHTML = arreyOrd;
+    }
 
     // posizione cognome utente
 } )
